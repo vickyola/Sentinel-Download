b7471b3cd1f5226557fa066febf52874e6b40ffe
Vicky <Viola.Wittekind@ufz.de>
2022-01-08 18:58:37 +0100

    unzip

--- download_sentinel.py
+++ download_sentinel.py
@@ -1,12 +1,11 @@
 from sentinelsat import SentinelAPI, make_path_filter 
 from sentinelsat import read_geojson
 from sentinelsat import geojson_to_wkt
-from datetime import date
-import pandas as pd
-import csv
+#import pandas as pd
 import os
-from os import path
-from os import getenv
+import glob
+#from os import path
+#from os import getenv
 from zipfile import ZipFile
 
 #run sentenv or enter username and pw here
@@ -37,22 +36,45 @@ def querydata(api, footprint, date, year, platformname = 'Sentinel-2', cloudcove
     return(query)
 
 def first_product_inlist(df):
-	df_sorted = df.head(2)
+	df_sorted = df.head(1)
 	return(df_sorted)
 
 def download_product(api, product, year):
-    path_filter = make_path_filter("*/IMG_DATA/*", exclude=False)  #only images
-    api.download_all(product.index, directory_path = 'downloads/' + str(year), nodefilter=path_filter) #what mean .index?        
+    product_name =  str(product['title'][0]) + '.zip'  #'downloads/' + str(year) +'/'+
+    #print(product_name )
+    if not os.path.exists(product_name ):#.os
+        api.download_all(product.index, directory_path = 'downloads/' + year ) #what mean .index?
+   
+    return(product_name)
+
+def extract_images(productname, year):
+    directory_path = 'downloads/' + year +'/' + productname 
+    archive = ZipFile(directory_path, 'r')
     
+   # print(archive.namelist())
+    for file in archive.namelist():
+        if 'IMG_DATA' in file:
+           # print(file)
+            archive.extract(file, year + '_Images')
 
 def main():
     api = api_connect(API_USER, API_PASSWORD) 
-    year  =  2019
+    year  =  2016
+    
     footprint = geojson_to_wkt(read_geojson(FOOTPRINT_PATH)) 
     query= querydata(api , footprint,(str(year)+'0101',str(year)+'1231'), year = str(year)) #querydata in excelshet
-    product= first_product_inlist(query)#weglassen wenn alle
-    return(download_product(api, product, year) )#not a zip archive because oonly parts of the product
-    
+    #product= first_product_inlist(query)#weglassen wenn alle
+    product_name = download_product(api, query, str(year)) #just table in zipfile not downlload to see how many pictures
+    return(extract_images(product_name , str(year)))
+    #zipfile = ZipFile(product_name, 'r')    
+   # zipfile.extractall(r'data_sentinel')
+    #zipfile.close()    
 if __name__ == "__main__":
 	main()
     
+
+
+#todo check whether zip is working
+#download whole product 
+#unpack?
+#extract images with glob
