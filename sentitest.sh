e43f8b837600ec9377baadae5063c37351bedd06
vickyola <56957983+vickyola@users.noreply.github.com>
2022-01-08 19:02:12 +0100

    Update sentitest.sh
    increase time

--- sentitest.sh
+++ sentitest.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=1gb
 #SBATCH --job-name=download_crop_model_data_test
 #SBATCH --output=/work/%u/%x-%j.out
-#SBATCH --time=0-00:01:00   
+#SBATCH --time=0-05:00:00   
 
 python download_sentinel.py
 
