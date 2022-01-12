68179c6edc1f2e434423cccedb643034bcf6895d
vickyola <56957983+vickyola@users.noreply.github.com>
2022-01-12 14:26:45 +0100

    Update sentitest.sh

--- sentitest.sh
+++ sentitest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --mem-per-cpu=1gb
-#SBATCH --job-name=download_crop_model_data_test
+#SBATCH --job-name=download_in_work_test
 #SBATCH --output=/work/%u/%x-%j.out
 #SBATCH --time=0-05:00:00   
 
